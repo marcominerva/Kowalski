07c281187145f39a9e55a1a82fc21a3cd81da9f7
Marco Minerva <marco.minerva@gmail.com>
2018-09-28 17:04:21 +0200

    Add config files for app and database

---
+++ config/JokesData.sql
@@ -0,0 +1,124 @@
+SET IDENTITY_INSERT [dbo].[Jokes] ON 
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (1, N'Che cosa hanno in comune un televisore e una formica? Le antenne!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (2, N'Qual è la città preferita dai ragni? Mosca!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (3, N'Qual è la pianta più puzzolente? Quella dei piedi!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (4, N'Che cos''è una zebra? Un cavallo evaso dal carcere!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (5, N'Sapete perché il pomodoro non riesce a dormire? Perché l’insalata… Russa!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (6, N'La maestra dice a un alunno: “Il tuo tema intitolato ''Il mio cane'' è uguale a quello di tuo fratello, l’hai copiato?” E lui: “No maestra, è che abbiamo lo stesso cane!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (7, N'Un alunno chiede alla maestra: “Maestra, posso essere punito per qualcosa che non ho fatto?”. E la maestra: “Certo che no! Perché me lo chiedi?”. Allora l''alunno risponde: “Perché non ho fatto i compiti!”.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (8, N'Un millepiedi a un altro millepiedi: “Come va tuo figlio a scuola?”. E l’altro: “Mah, mi sembra che la prenda un po’ sottogamba, sottogamba, sottogamba...”.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (9, N'Napoli. Un rapinatore sale sull’autobus e grida: “Fermi, questa è una rapina!”. Un signore si alza e dice: “Maronn’ mia, che spavento: pensavo fosse ‘o controllore!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (10, N'Una signora sale su un taxi e si raccomanda: “Faccia attenzione, che sono madre di otto figli!”.
+Il tassista si volta incredulo ed esclama: “E io dovrei stare attento??!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (11, N'Dottore, soffro di personalità multipla… vi prego, aiutateci!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (12, N'– Il dottore mi ha detto che devo smettere di giocare a pallone!
+– Ti ha trovato qualcosa?
+– No, ma mi ha visto giocare.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (13, N'Sai cosa beve l’elettricista? Solo birra alla spina.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (14, N'Prima facevo di mestiere al circo l´uomo proiettile. Ma non era un mestiere per un uomo del mio calibro.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (15, N'L’agopuntura è un ottimo rimedio medico. Non ho mai visto un porcospino ammalato.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (16, N'Il modo migliore di fare soldi navigando con la rete è di lavorare su un peschereccio.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (17, N'Il marinaio spiegò le vele al vento, ma il vento non capì.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (18, N'Un medico al paziente: – Dica tre volte trentatré.
+E il paziente: – Novantanove!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (19, N'Ma se il mio capo si droga, io sono un tossico-dipendente?')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (20, N'Il pittore: “Posso dipingere le sue mucche?”.
+Il contadino: “Perché non le piace il colore che hanno?”.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (21, N'“Dottore, soffro di vuoti di memoria. E le dirò anche un’altra cosa: soffro di vuoti di memoria”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (22, N'“Dottore, ho bisogno che lei mi prescriva un paio di occhiali!”
+“Questo é poco, ma sicuro! Solo che questa é una macelleria!!!"')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (23, N'– Dottore, ha qualcosa contro la tosse?
+– No, prego, faccia pure!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (24, N'Durante uno spettacolo comico: Bene, ho finito le battute che avevo in serbo. Adesso comincio con quelle in croato.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (25, N'Dall’ombrellaio – “Questo ombrello costa troppo! Per meno cosa posso prendere?”
+– “La pioggia!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (26, N'In un negozio d’abbigliamento. – Posso provare quel vestito in vetrina?
+– Beh, sarebbe meglio che lei lo provasse in camerino.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (27, N'Cosa fa un testimone con il raffreddore?
+Influenza la corte…')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (28, N'Dove va in vacanza una matita?
+Ovunque non ci sia un clima temperato.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (29, N'Perché in Transilvania ci sono tante risaie?
+Perché il riso fa buon sangue.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (30, N'Cosa ci fa una tv in mezzo al mare?
+Va in onda!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (31, N'Cosa disse la funga al fungo?
+Non fare il porcino!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (32, N'Cosa faceva uno sputo su una scala?
+Saliva.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (33, N'Sapete perché Capitano Uncino è morto?
+Perché si è fatto il bidé con la mano sbagliata')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (34, N'Perché le ballerine danzano sempre sulla punta dei piedi?
+Non sarebbe più semplice scritturare ballerine più alte?')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (35, N'Lei: “Tu non mi capisci…”.
+Lui: “Cosa intendi dire?”.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (36, N'La moglie al marito – “Caro… Tu preferisci una donna bella oppure una intelligente?”
+Lui: “Nessuna delle due cara… Ti amo così come sei!!!!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (37, N'Credevo di essere un amante eccezionale, finché non scoprii che lei aveva l’asma.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (38, N'Io mi ricordo sempre il compleanno di mia moglie. Cade il giorno dopo di quando lei me lo ricorda.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (39, N'Un’oca incontra un cane e gli dice: “Che freddo cane!”
+E il cane le risponde: “Non dirlo a me, io ho la pelle d’oca!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (40, N'Una coccinella entra in farmacia e chiede: ha niente per i punti neri?')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (41, N'Lo sapete perché le trote hanno le spine? Perché il fiume ha la corrente!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (42, N'Due serpenti parlano tra loro: “Quella vipera della mia ragazza mi ha lasciato… ma tornerà da me strisciando!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (43, N'Dove vanno a studiare i pesci? Sui banchi di sabbia…')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (44, N'Cosa fanno due api sulla luna? La luna di miele.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (45, N'Ero un bambino prodigio. Impiegavo sempre meno di sei mesi per fare i puzzle, anche se sulla scatola c’era scritto “Dai 2 ai 5 anni”…')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (46, N'Due spartiti musicali stanno litigando, interviene un terzo che dice: – Ragazzi andiamo… non c’è motivo!!!')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (47, N'Perché in America fa freddo? Perché é stata scoperta.')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (48, N'Qual è il contrario di “melodia”? “Se-lo-tenga”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (49, N'Cartello in un bar: “Chi beve per dimenticare è pregato di pagare in anticipo!”')
+GO
+INSERT [dbo].[Jokes] ([Id], [Content]) VALUES (50, N'“Mio figlio é cinque anni che suona al conservatorio, ma non gli hanno ancora aperto”')
+GO
+SET IDENTITY_INSERT [dbo].[Jokes] OFF
+GO
